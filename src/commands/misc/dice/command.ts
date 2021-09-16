cb88386a714c5a32509be1c6ccc1dc1219f8be70
Bryce Bryce <bpearce@angelo.edu>
2021-09-15 23:13:28 -0700

    roll command

--- src/commands/misc/dice/command.ts
+++ src/commands/misc/dice/command.ts
@@ -1,7 +1,7 @@
-// todo: implement https://www.npmjs.com/package/roll
-
 import { Command } from '../../../types/Command';
+import Roll from 'roll';
 
+const dice = new Roll();
 export const command: Command = {
     name: 'Dice Roll',
     description: 'Rolls dice',
@@ -9,7 +9,20 @@ export const command: Command = {
     args: true,
     enabled: true,
     usage: '[invocation] [dice_roll]',
-    async execute(message) {
-        message.channel.send('Not yet implemented');
+    async execute(message, args) {
+        const rollParams = args.join('');
+        if (!isValidArgs(rollParams)) return;
+        const roll = dice.roll(rollParams);
+
+        const sumTotal = roll.input.sides * roll.input.quantity;
+        const percentOfTotal = (100 * roll.result) / sumTotal;
+        message.channel.send(`:: Total ${roll.result} / ${sumTotal} [${Math.round(percentOfTotal)}%] :: Results [${roll.rolled.join(', ')}] ::`);
     }
+};
+
+const isValidArgs = (args: string) => {
+    if (args.length === 0) return false;
+
+    const digitsOnly = Number(args.replace(/\D/g, ''));
+    return Number.isInteger(digitsOnly);
 };
