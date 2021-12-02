bf9356e8d076dfb208ad7f472d0212bafe9629d8
Bryce Bryce <bpearce@angelo.edu>
2021-12-01 20:24:28 -0800

    format text + alt text

--- src/commands/misc/xkcd/command.ts
+++ src/commands/misc/xkcd/command.ts
@@ -1,70 +1,71 @@
-import { Command } from '../../../types/Command';
 import got from 'got';
+import { Command } from '../../../types/Command';
 
 interface XKCD {
-    "month": string,
-    "num": number,
-    "link": string,
-    "year": string,
-    "news": string,
-    "safe_title": string,
-    "transcript": string,
-    "alt": string,
-    "img": string,
-    "title": string,
-    "day": string
+  month: string;
+  num: number;
+  link: string;
+  year: string;
+  news: string;
+  safe_title: string;
+  transcript: string;
+  alt: string;
+  img: string;
+  title: string;
+  day: string;
 }
 
 const numberRegex = new RegExp(/^\d+$/);
 
 const command: Command = {
-    name: 'xkcd',
-    description: 'Gets xkcd comic',
-    invocations: ['xkcd'],
-    args: true,
-    enabled: true,
-    usage: '[invocation]',
-    async execute(message, args) {
-        let comic: XKCD;
-        try {
-            if (args.length === 0) {
-                comic = (await getLatestComic());
-            } else if (numberRegex.test(args[0])) {
-                comic = (await getSpecificComic(Number(args[0])));
-            } else {
-                comic = (await getRandomComic());
-            }
-            message.channel.send({ files: [comic.img] });
-        } catch (ex) {
-            message.channel.send(ex['message'] || 'Something went really wrong');
-        }
+  name: 'xkcd',
+  description: 'Gets xkcd comic',
+  invocations: ['xkcd'],
+  args: true,
+  enabled: true,
+  usage: '[invocation]',
+  async execute(message, args) {
+    let comic: XKCD;
+    try {
+      if (args.length === 0) {
+        comic = await getLatestComic();
+      } else if (numberRegex.test(args[0])) {
+        comic = await getSpecificComic(Number(args[0]));
+      } else {
+        comic = await getRandomComic();
+      }
+      await message.channel.send({ files: [comic.img] });
+      message.channel.send(`[${comic.alt}]`);
+    } catch (ex) {
+      message.channel.send(ex['message'] || 'Something went really wrong');
     }
+  },
 };
 
 const getLatestComic = async (): Promise<XKCD> => {
-    try {
-        return await got('https://xkcd.com/info.0.json').json() as XKCD;
-    } catch (ex) {
-        throw new Error('Failed to fetch from xkcd');
-    }
+  try {
+    return (await got('https://xkcd.com/info.0.json').json()) as XKCD;
+  } catch (ex) {
+    throw new Error('Failed to fetch from xkcd');
+  }
 };
 
 const getSpecificComic = async (comicNumber: number): Promise<XKCD> => {
-    try {
-        return await got(`https://xkcd.com/${comicNumber}/info.0.json`).json() as XKCD;
-    } catch (ex) {
-        throw new Error('Failed to fetch from xkcd');
-    }
+  try {
+    return (await got(`https://xkcd.com/${comicNumber}/info.0.json`).json()) as XKCD;
+  } catch (ex) {
+    throw new Error('Failed to fetch from xkcd');
+  }
 };
 
 const getRandomComic = async (): Promise<XKCD> => {
-    try {
-        const latest = await getLatestComic();
-        const randomChapterNumber = Math.floor(Math.random() * (latest.num - 1 + 1)) + 1;
-        return await getSpecificComic(randomChapterNumber);
-    } catch {
-        throw new Error('Failed to fetch from xkcd');
-    }
+  try {
+    const latest = await getLatestComic();
+    const randomChapterNumber = Math.floor(Math.random() * (latest.num - 1 + 1)) + 1;
+    return await getSpecificComic(randomChapterNumber);
+  } catch {
+    throw new Error('Failed to fetch from xkcd');
+  }
 };
 
 export default command;
