1b6ea805a4e12123a114d7fcd98da7a19cd66f7d
Carpogenic <carpogenic@gmail.com>
2023-09-23 00:56:17 +0200

    add much needed scooby functionality (#20)

---
+++ src/commands/misc/scooby/command.ts
@@ -0,0 +1,22 @@
+import { Command } from '../../../types/Command';
+
+const command: Command = {
+  name: 'Scooby',
+  description: 'Returns an image of Schoobert Doo',
+  invocations: ['scoob', 'scooby', 'scoobs'],
+  enabled: true,
+  args: false,
+  usage: '[invocation]',
+  async execute(message) {
+    const imageUrl = 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Scooby-Doo.png/150px-Scooby-Doo.png';
+    
+    try {
+      message.channel.send({ files: [imageUrl] });
+    } catch (error) {
+      console.error("Error sending scoob:", error);
+      message.reply('Scooby is on vacation');
+    }
+  }
+};
+
+export default command;
