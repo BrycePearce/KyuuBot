302856ee3ef9249fe06ed35690703fa54f23e83b
Bryce Bryce <bpearce@angelo.edu>
2023-12-29 15:55:39 -0800

    chat completion model change

--- src/commands/misc/kyuupt/chatCompletion/command.ts
+++ src/commands/misc/kyuupt/chatCompletion/command.ts
@@ -43,7 +43,7 @@ const command: Command = {
 
     try {
       const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-4-1106-preview',
         messages: [
           {
             role: 'system',
