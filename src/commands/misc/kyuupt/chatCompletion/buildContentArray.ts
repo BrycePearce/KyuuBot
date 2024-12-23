38b2b09a9e90dfc7120cf11c8ad826246c9ba92a
Bryce Bryce <bpearce@angelo.edu>
2024-12-22 18:47:15 -0800

    whoops

--- src/commands/misc/kyuupt/chatCompletion/buildContentArray.ts
+++ src/commands/misc/kyuupt/chatCompletion/buildContentArray.ts
@@ -13,6 +13,13 @@ export type OpenAIContent = TextContent | ImageContent;
 export function buildContentArray(userPrompt: string, imageUrls: string[]): OpenAIContent[] {
   const contentArray: OpenAIContent[] = [];
 
+  if (userPrompt.trim().length > 0) {
+    contentArray.push({
+      type: 'text',
+      text: userPrompt,
+    });
+  }
+
   imageUrls.forEach((imgUrl) => {
     contentArray.push({
       type: 'image_url',
