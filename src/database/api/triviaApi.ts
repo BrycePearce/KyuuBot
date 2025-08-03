49164b296d23f038d405d2c2b4aef905e23f94da
Bryce Bryce <bpearce@angelo.edu>
2025-08-03 02:47:27 -0700

    unused comment

--- src/database/api/triviaApi.ts
+++ src/database/api/triviaApi.ts
@@ -1,37 +1,18 @@
 import { getDbContext } from '..';
 import { TriviaStats } from '../entities';
 import { findOrCreateUser } from './userApi';
-/*
-userid - pts
-177112397872365568 42052
-226100196675682304 996
-182320583491452928 804
-85078157660524544  394
-220707155060326400 29
-203616025147604993 23
-225435115923177472 15
-785257081925926922 12
-217050307769794571 11
-816115581966090270 7
-234423492777345026 6
-136671110883180545 3
-*/
+
 export async function addPoints(channelId: string, authorId: string, pointsEarned: number) {
   const { em, triviaStatsRepository } = getDbContext();
-  console.log('in add pts');
   const user = await findOrCreateUser(authorId);
-  console.log('found or made user user', JSON.stringify(user));
   const trivia = await triviaStatsRepository.findOne({ channelId, user }, { populate: ['user'] });
-  console.log('meow meow found trivia', trivia);
   if (!trivia) {
-    console.log('not block');
     const userTriviaPoints = new TriviaStats();
     userTriviaPoints.channelId = channelId;
     userTriviaPoints.user = user;
     userTriviaPoints.points = pointsEarned;
     await em.persistAndFlush(userTriviaPoints);
   } else {
-    console.log('else block');
     trivia.points += pointsEarned;
     await em.persistAndFlush(trivia);
   }
