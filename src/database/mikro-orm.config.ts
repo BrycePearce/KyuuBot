1ff375030629fe817bd7ef26eb38b281c6aefa30
Bryce Bryce <bpearce@angelo.edu>
2025-08-03 01:49:31 -0700

    mikro orm 6 bad

---
+++ src/database/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { defineConfig } from '@mikro-orm/sqlite'; // even in v5
+import path from 'path';
+import { BaseEntity } from './entities/BaseEntity';
+import { Reminder } from './entities/Reminder';
+import { TriviaStats } from './entities/TriviaStats';
+import { User } from './entities/User';
+
+export default defineConfig({
+  type: 'sqlite',
+  dbName: './config/kyuu.db',
+  entities: [TriviaStats, User, Reminder, BaseEntity],
+  highlighter: new SqlHighlighter(),
+  debug: true,
+  migrations: {
+    path: path.join(process.cwd(), 'src/database/migrations'),
+  },
+});
